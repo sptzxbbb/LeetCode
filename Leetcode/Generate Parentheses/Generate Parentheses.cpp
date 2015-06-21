1d2a4cd28b26e5dec33ebf5088d86aecf7444f6d
sptzxbbb <sptzxbbb@gmail.com>
2015-06-21 14:32:01 +0800

    LeetCode

---
+++ Leetcode/Generate Parentheses/Generate Parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int left = 0, right = 0;
+        string s = "";
+        ans.clear();
+        generate(s, 0, 0, n);
+        return ans;
+    }
+
+    void generate(string& s, int left, int right, int n) {
+        if ((left + right) == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+        if (left < n) {
+            s += '(';
+            generate(s, left + 1, right, n);
+            s.pop_back();
+        }
+        if (right < left) {
+            s += ')';
+            generate(s, left, right + 1, n);
+            s.pop_back();
+        }
+        return;
+    }
+
+private:
+    vector<string> ans;
+};
+
