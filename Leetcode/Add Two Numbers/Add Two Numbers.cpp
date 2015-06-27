1a49ff0c7cc09b46e34488d068d603c777c1d6a4
sptzxbbb <sptzxbbb@gmail.com>
2015-06-27 18:27:37 +0800

    LeetCode

--- Leetcode/Add Two Numbers/Add Two Numbers.cpp
+++ Leetcode/Add Two Numbers/Add Two Numbers.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <stack>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
  struct ListNode {
@@ -11,52 +13,63 @@ using namespace std;
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        long num1 = 0, num2 = 0;
+        vector<int> digit1, digit2;
         ListNode *p1 = l1, *p2 = l2;
-        stack<int> digit1, digit2;
         while (p1 != NULL) {
-            digit1.push(p1->val);
+            digit1.push_back(p1->val);
             p1 = p1->next;
         }
-        while (!digit1.empty()) {
-            num1 = num1 * 10 + digit1.top();
-            digit1.pop();
-        }
-
         while (p2 != NULL) {
-            digit2.push(p2->val);
+            digit2.push_back(p2->val);
             p2 = p2->next;
         }
-        while (!digit2.empty()) {
-            num2 = num2 * 10 + digit2.top();
-            digit2.pop();
-        }
-        long ans = num1 + num2, remainder = 0;
-        ListNode* head = NULL, *last = head, *temp;
-        if (ans != 0) {
-            while (ans != 0) {
-                remainder = ans % 10;
-                ans /= 10;
-                if (head) {
-                    temp = new ListNode(remainder);
-                    last->next = temp;
-                    last = temp;
-                } else {
-                    head = new ListNode(remainder);
-                    last = head;
-                }
+
+
+        int size = min(digit1.size(), digit2.size());
+        // Addition
+        for (int i = 0; i < size; i++) {
+            if (digit1.size() >= digit2.size()) {
+                digit1[i] += digit2[i];
+            } else {
+                digit2[i] += digit1[i];
             }
+        }
+        vector<int> ans;
+        if (digit1.size() >= digit2.size()) {
+            ans = digit1;
         } else {
-            head = new ListNode(0);
+            ans = digit2;
+        }
+        int carry = 0;
+        // Deal with Carry
+        size = ans.size();
+        for (int i = 0; i < size; i++) {
+            ans[i] += carry;
+            carry = ans[i] / 10;
+            ans[i] %= 10;
+        }
+        if (carry) {
+            ans.push_back(1);
+        }
+        size = ans.size();
+        ListNode *head, *last, *temp;
+        for (int i = 0; i < size; i++) {
+            if (0 == i) {
+                head = new ListNode(ans[i]);
+                last = head;
+            } else {
+                temp = new ListNode(ans[i]);
+                last->next = temp;
+                last = temp;
+            }
         }
         return head;
     }
 };
 
 int main(int argc, char *argv[]) {
-    ListNode a1(9), b1(1), b2(9), b3(9), ;
+    ListNode a1(0), b1(7), b2(3);
     b1.next = &b2;
-    b2.next = &b3;
     Solution k;
     k.addTwoNumbers(&a1, &b1);
     return 0;
