f086a41be2a57ba243a049d5f6b0ae6ae942ccca
sptzxbbb <sptzxbbb@gmail.com>
2015-07-19 03:48:43 +0800

    LeetCode

---
+++ Leetcode/N-Queens II/N-Queens II.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+         vector<string> board;
+        // initial the board
+        bool col_check[n];
+        for (int i = 0; i < n; i++) {
+            string row(n, '.');
+            board.push_back(row);
+            col_check[i] = true;
+        }
+        dfs(board, 0, n, col_check);
+        return result.size();
+    }
+
+    void dfs(vector<string>& board, int row, int n, bool* col_check) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (col_check[col] &&
+                check_diagnoal(board, row, col, n)) {
+                board[row][col] = 'Q';
+                col_check[col] = false;
+                dfs(board, row + 1, n, col_check);
+                col_check[col] = true;
+                board[row][col] = '.';
+            }
+        }
+        return;
+    }
+
+    bool check_diagnoal(vector<string>& board, int row, int col, int n) {
+        int left = col, right = col;
+        for (int _row = row - 1; _row >= 0; _row--) {
+            --left;
+            ++right;
+            if (left >= 0 && 'Q' == board[_row][left]) {
+                return false;
+            }
+            if (right <= n - 1 && 'Q' == board[_row][right]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:
+    vector<vector<string> > result;
+};
