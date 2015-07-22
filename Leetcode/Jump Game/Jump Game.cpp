1f88d8ed9f237e18e9c150c4d1e144a43a237ee6
sptzxbbb <sptzxbbb@gmail.com>
2015-07-22 20:26:46 +0800

    LeetCode

--- Leetcode/Jump Game/Jump Game.cpp
+++ Leetcode/Jump Game/Jump Game.cpp
@@ -6,17 +6,17 @@ public:
         while (pos < size - 1 && nums[pos] != 0) {
             max = -1;
             for (int i = pos + 1; i <= (pos + nums[pos]); i++) {
-                if (i >= size - 1 || i + nums[i] >= size - 1) {
+                if (i >= size - 1) {
                     next = size - 1;
                     break;
-                } else if ((i + nums[i] > max)) {
+                } else if ((i + nums[i] >= max)) {
                     next = i;
                     max = i + nums[i];
                 }
             }
             pos = next;
         }
-        if (pos >= size - 1) {
+        if (size - 1 == pos) {
             return true;
         } else {
             return false;
