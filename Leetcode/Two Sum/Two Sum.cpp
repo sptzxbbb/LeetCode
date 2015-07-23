1fa58833fbeb30ef18d78a87d034e39bda88b3a5
sptzxbbb <sptzxbbb@gmail.com>
2015-07-23 15:46:08 +0800

    LeetCode

---
+++ Leetcode/Two Sum/Two Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> value;
+        vector<int> result;
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            value[nums[i]] = i;
+        }
+        for (int i = 0; i < size; i++) {
+            int k = target - nums[i];
+            map<int, int>::iterator it;
+            it = value.find(k);
+            if (it != value.end() && it->second != i) {
+                result.push_back(i + 1);
+                result.push_back(value[k] + 1);
+                break;
+            }
+        }
+        return result;
+    }
+};
+
