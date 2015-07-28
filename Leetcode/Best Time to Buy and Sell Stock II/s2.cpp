4a832e7bb04087c4a77b9b2aa54a406d93baccb6
sptzxbbb <sptzxbbb@gmail.com>
2015-07-28 16:04:54 +0800

    LeetCode

---
+++ Leetcode/Best Time to Buy and Sell Stock II/s2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int size = prices.size();
+        for (int i = 1; i < size; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
+};
