e575532b56dd710a7e107d0ec75f2ce76c350cae
sptzxbbb <sptzxbbb@gmail.com>
2015-08-10 15:45:01 +0800

    LeetCode

---
+++ Leetcode/Maximum Gap/Maximum Gap.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int size = nums.size();
+        if (size < 2) {
+            return 0;
+        }
+        int maxNum = nums[0];
+        int minNum = nums[0];
+        for (int i = 1; i < size; i++) {
+            maxNum = max(maxNum, nums[i]);
+            minNum = min(minNum, nums[i]);
+        }
+        int len = ceil((floor)(maxNum - minNum) / (floor)(size - 1));
+        vector<vector<int> > bucket((maxNum - minNum) / len + 1);
+        for (int i = 0; i < size; i++) {
+            int k = (nums[i] - minNum) / len;
+            if (bucket[k].empty()) {
+                bucket[k].push_back(nums[i]);
+                bucket[k].push_back(nums[i]);
+            } else {
+                bucket[k][0] = min(bucket[k][0], nums[i]);
+                bucket[k][1] = max(bucket[k][1], nums[i]);
+            }
+        }
+        int gap = 0;
+        int prev = -1;
+        size = bucket.size();
+        for (int i = 1; i < size; i++) {
+            if (bucket[i].empty()) {
+                continue;
+            }
+            if (-1 == prev) {
+                prev = i;
+            }
+            gap = max(gap, bucket[i][0] - bucket[prev][1]);
+            prev = i;
+        }
+        return gap;
+    }
+};
