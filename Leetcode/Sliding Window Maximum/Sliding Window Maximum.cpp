7aa68c839fc4afc953f525748ec30c825408aa89
sptzxbbb <sptzxbbb@gmail.com>
2015-08-13 11:51:33 +0800

    LeetCode

---
+++ Leetcode/Sliding Window Maximum/Sliding Window Maximum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <deque>
+#include "vector"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        int size = nums.size();
+        int i = 0;
+        vector<int> res;
+        while (i < size) {
+            if (q.size() && i - q.front() > k - 1) {
+                q.pop_front();
+            }
+            // deal with ith element
+            while (q.size() && nums[q.back()] <= nums[i]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+            if (i >= k - 1) {
+                res.push_back(nums[q.front()]);
+            }
+            ++i;
+        }
+        return res;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    int a[] = {1, 3, 1, 2, 0, 5};
+    vector<int> v(&a[0], &a[5]);
+    Solution k;
+    k.maxSlidingWindow(v, 3);
+    return 0;
+}
