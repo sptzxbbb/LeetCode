100669b66abc41e6fc764db9e6e1415505789662
sptzxbbb <sptzxbbb@gmail.com>
2015-08-16 00:25:52 +0800

    LeetCode

---
+++ Leetcode/Candy/Candy.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int size = ratings.size();
+        vector<int> candy(size);
+        candy[0] = 1;
+        for (int i = 1; i < size; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            } else {
+                candy[i] = 1;
+            }
+        }
+        int sum = candy[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+                candy[i] = candy[i + 1] + 1;
+            }
+            sum += candy[i];
+        }
+        return sum;
+    }
+};
