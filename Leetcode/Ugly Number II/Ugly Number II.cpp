c9134da393c66a476cf6a528037bb1cb9baa2cba
sptzxbbb <sptzxbbb@gmail.com>
2015-08-30 00:00:42 +0800

    LeetCode

---
+++ Leetcode/Ugly Number II/Ugly Number II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> res(1, 1);
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (res.size() < n) {
+            int m2 = res[i2] * 2, m3 = res[i3] * 3, m5 = res[i5] * 5;
+            int minimal = min(m2, min(m3, m5));
+            if (minimal == m2) {
+                ++i2;
+            }
+            if (minimal == m3) {
+                ++i3;
+            }
+            if (minimal == m5) {
+                ++i5;
+            }
+            res.push_back(minimal);
+        }
+        return res.back();
+    }
+};
