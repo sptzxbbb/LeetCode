6c55dfaec4d661812b5931cd726c8bc617a789a5
sptzxbbb <sptzxbbb@gmail.com>
2015-08-30 22:39:28 +0800

    LeetCode

--- Leetcode/Validate Binary Search Tree/Validate Binary Search Tree.cpp
+++ Leetcode/Validate Binary Search Tree/Validate Binary Search Tree.cpp
@@ -1,28 +1,66 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// class Solution1 {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         valid(root, LONG_MIN, LONG_MAX);
+//     }
+//     bool valid(TreeNode* node, long MIN, long MAX) {
+//         if (node == NULL) {
+//             return true;
+//         }
+//         if (node->val > MIN &&
+//             node->val < MAX &&
+//             valid(node->left, MIN, node->val) &&
+//             valid(node->right, node->val, MAX)) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        valid(root, LONG_MIN, LONG_MAX);
+        vector<int> v;
+        inorder(v, root);
+        for (int i = 0; i < (int)v.size() - 1; i++) {
+            if (v[i] >= v[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
-    bool valid(TreeNode* node, long MIN, long MAX) {
-        if (node == NULL) {
-            return true;
+
+    void inorder(vector<int>& v, TreeNode* root) {
+        if (root == NULL) {
+            return;
         }
-        if (node->val > MIN &&
-            node->val < MAX &&
-            valid(node->left, MIN, node->val) &&
-            valid(node->right, node->val, MAX)) {
-            return true;
-        } else {
-            return false;
-        }
+        inorder(v, root->left);
+        v.push_back(root->val);
+        inorder(v, root->right);
+        return;
     }
 };
+
+int main(int argc, char *argv[])
+{
+    TreeNode* root = NULL;
+    Solution k;
+    k.isValidBST(root);
+    return 0;
+}
