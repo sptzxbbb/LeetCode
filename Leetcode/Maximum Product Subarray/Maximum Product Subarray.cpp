c395d810ec08536b7bdeb2e5716493d59b75025b
sptzxbbb <sptzxbbb@gmail.com>
2015-09-02 16:00:05 +0800

    LeetCode

--- Leetcode/Maximum Product Subarray/Maximum Product Subarray.cpp
+++ Leetcode/Maximum Product Subarray/Maximum Product Subarray.cpp
@@ -1,6 +1,15 @@
 class Solution {
-  public:
+public:
     int maxProduct(vector<int>& nums) {
-        
+    int ans = nums[0], mini = nums[0], maxn = nums[0];
+    int size = nums.size();
+    for (int i = 1; i < size; ++i) {
+      int a = min(mini * nums[i], maxn * nums[i]);
+      int b = max(mini * nums[i], maxn * nums[i]);
+      mini = min(nums[i], a);
+      maxn = max(nums[i], b);
+      ans = max(ans, maxn);
     }
-}
+    return ans;
+  }
+};
