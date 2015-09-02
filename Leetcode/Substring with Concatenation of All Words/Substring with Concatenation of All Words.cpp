d1f3f226f015913a3c90ef2359f6401287b2a959
sptzxbbb <sptzxbbb@gmail.com>
2015-09-02 16:54:24 +0800

    LeetCode

--- Leetcode/Substring with Concatenation of All Words/Substring with Concatenation of All Words.cpp
+++ Leetcode/Substring with Concatenation of All Words/Substring with Concatenation of All Words.cpp
@@ -11,11 +11,11 @@ class Solution {
             ++wordCount[words[i]];
         }
         map<string, int> sCount;
-        int wordSize = words[0].size();
-        for (int i = 0; i <= (int)s.size() - wordsSize * wordSize; i++) {
+        int wordLen = words[0].size();
+        for (int i = 0; i <= (int)s.size() - wordsSize * wordLen; i++) {
             sCount.clear();
             for (int j = 0; j < wordsSize; j++) {
-                string subWord = s.substr(i + j * wordSize, wordSize);
+                string subWord = s.substr(i + j * wordLen, wordLen);
                 if (wordCount.find(subWord) != wordCount.end()) {
                     ++sCount[subWord];
                     if (sCount[subWord] > wordCount[subWord]) {
@@ -33,24 +33,34 @@ class Solution {
     }
 };
 
+
+// We can leverage the fact that the strings in words have the same length to speed up our algorithm.
+// We can divide all the possible starting points (i.e. 0..sLen-1) into wordL sequences and the i-th sequence is (i, i+wordL, i+2*wordL, ...) (I=0..wordL-1).
+// Each time we scan one sequence (for j loop). First, we need to build a dictionary based on words and unordered_map is used to simplify look-up operation (key is string, value is count).
+// Then for each sequence starting at i, we check if s[i..i+wordL-1] is a word in dict, if no (which means it is not a word in words), then we reset count (the number of words we found in the current window starting at start), and move start to i+wordL to skip the current word and also we need to recover dict.
+// If s[i..i+wordL-1] is a word in dict, then we check if we already found all such words before in the current window (i.e. --dict[s.substr(i, wordL)] < 0). If so, the current one is a redundant one, then we have to move forward start to remove one occurence of the current word (i.e. to make dict[s.substr(i, wordL)] == 0) so that the current word can be included in the window.
+// We also need to update count accordingly.
+// If we find all the words in the current window ( i.e. (++count == wSize)), then we save start to res and move start to remove one occurence of the current word to continue the search. We repeat the above process for each word sequence (i.e. each i). remember after each i iteration, we need to recover dict.
+
 class Solution {
   public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int sLen = s.size(), wSize, wordL, i, j, count, start ;
+        int sLen = s.size(), wSize, wordL, i, j, count, start;
         vector<int> res;
-        if(!(wSize=words.size()) || !(wordL = words[0].size()) || sLen < wSize*wordL) return res;
-        unordered_map<string, int> dict;
-        //build dictory
-        for(i=0;i<wSize; ++i) ++dict[words[i]];
+        if(!(wSize=words.size()) ||
+           !(wordL = words[0].size()) ||
+           sLen < wSize*wordL) return res;
 
-        for(j=0; j<wordL;++j) // go through each possible starting point of a word sequence
-        {
-            for(i=j, start = j, count=0; i<=sLen-wordL; i+=wordL) // go through each word sequence, remember to reset count and start
-            {
-                if(dict.count(s.substr(i, wordL))==0)
-                {// if it is not a word in words, reset start, count and dict
-                    while(start<i)
-                    {
+        unordered_map<string, int> dict;
+        //build dictionary
+        for(i=0;i<wSize; ++i) ++dict[words[i]];
+        // go through each possible starting point of a word sequence
+        for(j=0; j<wordL;++j) {
+            // go through each word sequence, remember to reset count and start
+            for(i=j, start = j, count=0; i<=sLen-wordL; i+=wordL) {
+                // if it is not a word in words, reset start, count and dict
+                if(dict.count(s.substr(i, wordL))==0) {
+                    while(start<i) {
                         ++dict[s.substr(start, wordL)];
                         start +=wordL;
                     }
@@ -58,8 +68,8 @@ class Solution {
                     count = 0;
                     continue;
                 }
-                if(--dict[s.substr(i, wordL)] < 0)
-                {// if it is a word in words, but we already found such words, then we need to move forward start to exclude such extra word
+                // if it is a word in words, but we already found such words, then we need to move forward start to exclude such extra word
+                if(--dict[s.substr(i, wordL)] < 0) {
                     while(dict[s.substr(i, wordL)]<0)
                     {
                         ++dict[s.substr(start, wordL)];
@@ -67,8 +77,8 @@ class Solution {
                         --count;
                     }
                 }
-                if(++count == wSize)
-                { // otherwise, we found a valid word in words, update count and if we find all the words, then save the results, and move start
+                // otherwise, we found a valid word in words, update count and if we find all the words, then save the results, and move start
+                if(++count == wSize) {
                     res.push_back(start);
                     ++dict[s.substr(start, wordL)];
                     start +=wordL;
@@ -76,7 +86,9 @@ class Solution {
                 }
             }
             //recover dict for the next word sequence
-            while(start<i) {++dict[s.substr(start, wordL)]; start+=wordL;}
+            while(start<i) {
+                ++dict[s.substr(start, wordL)]; start+=wordL;
+            }
         }
     }
 };
