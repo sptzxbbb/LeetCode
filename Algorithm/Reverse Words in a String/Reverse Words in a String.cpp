e73aef9701f446f93606ddd0f96699f30227865e
sptzxbbb <sptzxbbb@gmail.com>
2015-09-10 00:12:53 +0800

    LeetCode

--- Algorithm/Reverse Words in a String/Reverse Words in a String.cpp
+++ Algorithm/Reverse Words in a String/Reverse Words in a String.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     void reverseWords(string &s) {
-        
+        stack<string> t;
+        int index = 0, size = s.size();
+        string temp = "";
+        while (index < size) {
+            if (s[index] != ' ') {
+                temp += s[index];
+            } else {
+                if (!temp.empty()) {
+                    t.push(temp);
+                    temp.clear();
+                }
+            }
+            ++index;
+        }
+        if (!temp.empty()) {
+            t.push(temp);
+            temp.clear();
+        }
+        string res;
+        while (!t.empty()) {
+            res += t.top() + " ";
+            t.pop();
+        }
+        if (!res.empty()) res.pop_back();
+        s = res;
     }
 };
