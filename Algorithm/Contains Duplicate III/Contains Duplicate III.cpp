50b15f06ca0a90dc03b3188e755478eec110ed25
sptzxbbb <sptzxbbb@gmail.com>
2015-09-18 17:27:39 +0800

    LeetCode

--- Algorithm/Contains Duplicate III/Contains Duplicate III.cpp
+++ Algorithm/Contains Duplicate III/Contains Duplicate III.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        
+        multiset<int> bst;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) {
+                bst.erase(nums[i - k - 1]);
+            }
+            auto lb = bst.lower_bound(nums[i] - t);
+            if (lb != bst.end() && abs(*lb - nums[i]) <= t) {
+                return true;
+            }
+            bst.insert(nums[i]);
+        }
+        return false;
     }
 };
