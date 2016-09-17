d59a1e41dea39035f9b77eacb23db5a3ddb99f91
sptzxbbb <sptzxbbb@gmail.com>
2016-09-18 03:28:59 +0800

    python algo 238

---
+++ LeetCode/Algorithms/python/238. Product of Array Except Self/.#main.py
@@ -0,0 +1,1 @@
+peter@peter-Inspiron-7537.9489:1474117852
---
+++ LeetCode/Algorithms/python/238. Product of Array Except Self/main.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for idx, val in enumerate(nums):
+            if idx == 0:
+                ans.append(1)
+            else:
+                ans.append(ans[idx - 1] * nums[idx - 1])
+
+        print(ans)
+
+        length = len(nums)
+        for idx, val in enumerate(reversed(nums)):
+            if idx == 0:
+                right_product = 1
+            else:
+                right_product *= nums[length - 1 - (idx - 1)]
+                ans[length - 1 - idx] *= right_product
+            
+        return ans
+
+
+def main():
+    s = Solution()
+    nums = [1, 2, 3, 4, 5]
+    ans = s.productExceptSelf(nums)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
