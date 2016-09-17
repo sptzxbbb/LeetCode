aa512526558fe304b0c0503d8efb2afcb9116845
sptzxbbb <sptzxbbb@gmail.com>
2016-09-18 02:03:20 +0800

    python alo 283

---
+++ LeetCode/Algorithms/python/283. Move Zeroes/main.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        non_zero_count = 0
+        for idx, val in enumerate(nums):
+            if val:
+                nums[non_zero_count] = nums[idx]
+                non_zero_count += 1
+
+        # nums[zero_count:] =
+        nums[non_zero_count:] = [0 for x in range(len(nums) - non_zero_count)]
+        return
+
+def main():
+    s = Solution()
+    nums = [1]
+    s.moveZeroes(nums)
+    print(nums)
+
+if __name__ == '__main__':
+    main()
