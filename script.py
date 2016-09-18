fae84d89fcda8f9a5800ae3421e023eb371a9088
sptzxbbb <sptzxbbb@gmail.com>
2016-09-18 15:58:31 +0800

    Add script to manage files

---
+++ script.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+
+def create_file():
+    for i in range(10):
+        filename = str(i) + ".txt"
+        f = open(filename, "w")
+    return
+
+
+def main():
+    files = glob.glob('./**/*.txt', recursive=True)
+    list(map(os.remove, files))
+
+if __name__ == '__main__':
+    main()
